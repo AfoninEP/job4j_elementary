21646fb3ce797ffbea18466ccdb702ab199fc5c7
zhenek <Afonin55@yandex.ru>
2025-03-11 13:07:14 +0300

    Заголовок в файле java [#505272]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class Info {
-    public static void main (String[] args){
-        System.out.println("11.03.2025");
+    public static void main(String[] args) {
+        System.out.println("19.01.2020");
     }
 }
