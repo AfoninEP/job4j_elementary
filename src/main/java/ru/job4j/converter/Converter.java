b2cc610baccce9c7d5a8531a9100df6aa99a20ce
zhenek <Afonin55@yandex.ru>
2025-03-12 22:35:47 +0300

    1. Модульные тесты [#505277]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -3,13 +3,10 @@
  * Assuming the conversion rate is 1 Dollar = 75 Rubles
  */
 package ru.job4j.converter;
-
 public class Converter {
-
     public static double rubleToEuro(double value) {
         return value / 90;
     }
-
     public static double rubleToDollar(double value) {
         return value / 75;
     }
--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -6,15 +6,15 @@ import static org.assertj.core.api.Assertions.*;
 class ConverterTest {
     @Test
     void whenConvert140RblThen2dotEuro() {
-        float input = 140f;
-        float expected = 2f;
+        float input = 140;
+        float expected = 2;
         float output = (float) Converter.rubleToEuro(input);
         float value = 0.0001f;
         assertThat(output).isEqualTo(expected, withPrecision(value));
     }
     @Test
     void whenConvert200RblThen2dot2222Euro() {
-        float input = 200f;
+        float input = 200;
         float expected = 2.2222f;
         float output = (float) Converter.rubleToEuro(input);
         float value = 0.0001f;
@@ -22,16 +22,16 @@ class ConverterTest {
     }
     @Test
     void whenConvert180RblThen3dotDollar() {
-        float input = 180f;
-        float expected = 3f;
+        float input = 180;
+        float expected = 3;
         float output = (float) Converter.rubleToDollar(input);
         float value = 0.0001f;
         assertThat(output).isEqualTo(expected, withPrecision(value));
     }
     @Test
     void whenConvert150RblThen2dotDollar() {
-        float input = 150f;
-        float expected = 2f;
+        float input = 150;
+        float expected = 2;
         float output = (float) Converter.rubleToDollar(input);
         float value = 0.0001f;
         assertThat(output).isEqualTo(expected, withPrecision(value));
