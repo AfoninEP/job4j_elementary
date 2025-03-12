a31dd111486cd31e8d059c7cf31caed1800a06cb
zhenek <Afonin55@yandex.ru>
2025-03-12 21:34:21 +0300

    1. Модульные тесты [#505277]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -13,6 +13,7 @@ class ConverterTest {
         float value = 0.0001f;
         assertThat(output).isEqualTo(expected, withPrecision(value));
     }
+
     @Test
     void whenConvert200RblThen2dot2222Euro() {
         float input = 200;
