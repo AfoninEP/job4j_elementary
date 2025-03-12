38e9196fa50bd3fbfdfe3ecaf2825839352eae74
zhenek <Afonin55@yandex.ru>
2025-03-12 22:32:19 +0300

    1. Модульные тесты [#505277]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -4,37 +4,34 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 class ConverterTest {
-
     @Test
-    void whenConvert140RblThen2Euro() {
-        float input = 140;
-        float expected = 2;
+    void whenConvert140RblThen2dotEuro() {
+        float input = 140f;
+        float expected = 2f;
         float output = (float) Converter.rubleToEuro(input);
         float value = 0.0001f;
         assertThat(output).isEqualTo(expected, withPrecision(value));
     }
-
     @Test
     void whenConvert200RblThen2dot2222Euro() {
-        float input = 200;
+        float input = 200f;
         float expected = 2.2222f;
         float output = (float) Converter.rubleToEuro(input);
         float value = 0.0001f;
         assertThat(output).isEqualTo(expected, withPrecision(value));
     }
-
     @Test
-    void whenConvert180RblThen3Dollar() {
-        float input = 180;
-        float expected = 3;
+    void whenConvert180RblThen3dotDollar() {
+        float input = 180f;
+        float expected = 3f;
         float output = (float) Converter.rubleToDollar(input);
         float value = 0.0001f;
         assertThat(output).isEqualTo(expected, withPrecision(value));
     }
     @Test
-    void whenConvert150RblThen2Dollar() {
-        float input = 150;
-        float expected = 2;
+    void whenConvert150RblThen2dotDollar() {
+        float input = 150f;
+        float expected = 2f;
         float output = (float) Converter.rubleToDollar(input);
         float value = 0.0001f;
         assertThat(output).isEqualTo(expected, withPrecision(value));
