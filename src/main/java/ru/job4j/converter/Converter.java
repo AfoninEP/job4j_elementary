ada55158cf676d0cec7fc7da3ef6d364a0a56bcc
zhenek <Afonin55@yandex.ru>
2025-03-13 05:59:46 +0300

    1. Модульные тесты [#505277]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -3,10 +3,12 @@
  * Assuming the conversion rate is 1 Dollar = 75 Rubles
  */
 package ru.job4j.converter;
+
 public class Converter {
     public static double rubleToEuro(double value) {
         return value / 90;
     }
+
     public static double rubleToDollar(double value) {
         return value / 75;
     }
