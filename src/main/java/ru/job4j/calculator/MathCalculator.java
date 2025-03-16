15a40cfa9ba6ae7ba200a1cf7d3e81b5d128b96f
zhenek <Afonin55@yandex.ru>
2025-03-16 22:19:15 +0300

    1.1. Что такое import [#505278]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -5,18 +5,18 @@ import ru.job4j.math.MathFunction;
 public class MathCalculator {
 
     public static double differenceAnddivision (double first, double second) {
-        return MathFunction.difference (first, second)
-                + MathFunction.division (first, second);
+        return MathFunction.difference(first, second)
+                + MathFunction.division(first, second);
     }
 
-    public static double sumall (double first, double second) {
-        return MathFunction.sum (first, second)
-                + MathFunction.multiply (first, second)
-                + MathFunction.difference (first, second)
-                + MathFunction.division (first, second);
+    public static double sumall(double first, double second) {
+        return MathFunction.sum(first, second)
+                + MathFunction.multiply(first, second)
+                + MathFunction.difference(first, second)
+                + MathFunction.division(first, second);
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("Результат расчета суммы первого метода равен: " + differenceAnddivision(10, 20));
         System.out.println("Результат расчета суммы второго метода равен: " + sumall(10, 20));
     }
