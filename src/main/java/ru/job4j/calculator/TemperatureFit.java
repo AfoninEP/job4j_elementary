76a43dec58f6051bdc26d13e55937a17f8390a07
zhenek <Afonin55@yandex.ru>
2025-03-17 23:41:48 +0300

    Тестирование Класса Калькулятора Температуры Хранения [#505279]

---
+++ src/main/java/ru/job4j/calculator/TemperatureFit.java
@@ -0,0 +1,25 @@
+/**
+* Ideal temperature for storing fruits in Celsius
+* Ideal temperature for storing meat in Celsius
+* Example room temperature
+*/
+package ru.job4j.calculator;
+
+public class TemperatureFit {
+
+    public static double idealTemperatureForFruits(int temperature) {
+        return temperature - 2.0;
+    }
+
+    public static double idealTemperatureForMeat(int temperature) {
+        return temperature - 5.0;
+    }
+
+    public static void main(String[] args) {
+        int temperature = 10;
+        double fruitsTemp = TemperatureFit.idealTemperatureForFruits(temperature);
+        double meatTemp = TemperatureFit.idealTemperatureForMeat(temperature);
+        System.out.println("Ideal temperature for fruits is " + fruitsTemp + "°C");
+        System.out.println("Ideal temperature for meat is " + meatTemp + "°C");
+    }
+}
---
+++ src/test/java/ru/job4j/calculator/TemperatureFitTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TemperatureFitTest {
+
+    @Test
+    void idealTemperatureForFruits() {
+    }
+
+    @Test
+    void idealTemperatureForMeat() {
+    }
+
+    @Test
+    void main() {
+    }
+}
