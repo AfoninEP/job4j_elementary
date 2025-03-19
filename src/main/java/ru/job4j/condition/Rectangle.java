5d6a9243dea56864e5c290bdab92d674265c3955
zhenek <Afonin55@yandex.ru>
2025-03-19 08:54:10 +0300

    Тестирование Класса Вычисления Диагонали Прямоугольника [#505281]

--- src/main/java/ru/job4j/condition/Rectangle.java
+++ src/main/java/ru/job4j/condition/Rectangle.java
@@ -1,13 +1,12 @@
-/**
+/*
 * Вычисление диагонали прямоугольника по теореме Пифагора
-*/
+**/
 
 package ru.job4j.condition;
 
 public class Rectangle {
 
     public static double diagonal(double length, double width) {
-
         return Math.sqrt(Math.pow(length, 2) + Math.pow(width, 2));
     }
 
